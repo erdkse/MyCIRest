117a2b604c2a3bb30cdea5b6774b093e998f9644
erdkse <erdikose8@hotmail.com>
2015-02-13 18:10:40 +0200

    new functions added

--- php/application/controllers/api/example.php
+++ php/application/controllers/api/example.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Example
@@ -11,69 +13,107 @@
  * @category	Controller
  * @author		Phil Sturgeon
  * @link		http://philsturgeon.co.uk/code/
-*/
-
+ */
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
-require APPPATH.'/libraries/REST_Controller.php';
+require APPPATH . '/libraries/REST_Controller.php';
 
-class Example extends REST_Controller
-{
-	function __construct()
-    {
+class Example extends REST_Controller {
+
+    function __construct() {
         // Construct our parent class
         parent::__construct();
-        
-        // Configure limits on our controller methods. Ensure
-        // you have created the 'limits' table and enabled 'limits'
-        // within application/config/rest.php
-        $this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
-        $this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
-        $this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
-
-        //$this->load->model(array('users_model','device_model','test_model'));
     }
 
-    function registerUser_post()
-    {
-        $user = array('email' => $this->input->post('email'),
-                        'password' => $this->input->post('password') );
+    function registerUser_post() {
 
-        $user_meta = array('firstname' => $this->input->post('firstname'),
-                            'lastname' => $this->input->post('lastname'),
-                            'mobile_number' => $this->input->post('firstname') );
+        //$username = $this->input->post('username')
+        $password = $this->input->post('password');
+        $email = $this->input->post('email');
+        $additional_data = array(
+            'first_name' => $this->input->post('first_name'),
+            'last_name' => $this->input->post('last_name'),
+            'company' => $this->input->post('company'),
+            'phone' => $this->input->post('phone'),
+            'ip_address' => $this->input->ip_address()
+        );
 
-        $insertion = $this -> users_model -> registerUser($user,$user_meta);
+        // 1 for passengers
+        // 2 for drivers
+        $group = $this->input->post('group');
 
-        if ($insertion) {
+        // -1 : account_creation_duplicate_email
+        // -2 : account_creation_duplicate_username
+        // -3 : Unknown reason
+        // id : SUCCESS!
+
+        $return = $this->users_model->registerUser($password, $email, $additional_data, $group);
+
+        /**/
+        if ($return > 0) {
             $this->response(array('code' => 200,
-                                    'status' => true,
-                                    'sonuc' => $sonuc), 200);
+                'status' => true,
+                'message' => "Succes",
+                'response' => $return), 200);
         } else {
-            $this->response(array(  'code' => 201,
-                                    'status' => true,
-                                    'sonuc' => $sonuc), 200);
+            switch ($return) {
+                case -1:
+                    $this->response(array('code' => 201,
+                        'status' => false,
+                        'message' => "account_creation_duplicate_email",
+                        'response' => $return), 200);
+                    break;
+                case -2:
+                    $this->response(array('code' => 202,
+                        'status' => false,
+                        'message' => "account_creation_duplicate_username",
+                        'response' => $return), 200);
+                    break;
+                case -3:
+                    $this->response(array('code' => 205,
+                        'status' => false,
+                        'message' => "Unknown reason",
+                        'response' => $return), 200);
+                    break;
+            }
         }
-        
     }
 
+    function loginUser_post() {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
 
-    function getUsers_post()
-    {
-        $userId = $this->input->post('user_id');
-        $user = $this->users_model->checkUserId($userId);
+        $query = $this->users_model->loginUser($email, $password);
 
-        /*
-        $message = array(
-                            'id' => $this->get('id'), 
-                            'name' => $this->post('name'), 
-                            'email' => $this->post('email'), 
-                            'message' => 'ADDED!');
+        if ($query) {
+            $this->response(array('code' => 200,
+                                    'status' => TRUE,
+                                    'message' => "login is successed",
+                                    'user' => $query), 200);
+        } else {
+            $this->response(array('code' => 201,
+                                    'status' => FALSE,
+                                    'message' => "login is failed",
+                                    'user' => $query), 200);
+        }
+    }
 
-        $this->getFeedsToArray($user);
-        $this->response($this->getFeedsToArray($user),200);
-        */
-        $this->response($user, 200); // 200 being the HTTP response code
-         
+    function logoutUser_post() {
+        $id = $this->input->post('id');
+        $query = $this->users_model->logoutUser($id);
+
+        if ($query) {
+            $this->response(array('code' => 200,
+                                    'status' => TRUE,
+                                    'message' => "logout is successed"), 200);
+        } else {
+            $this->response(array('code' => 201,
+                                    'status' => FALSE,
+                                    'message' => "logout is failed"), 200);
+        }
     }
     
+    
+
 }
+
+?>
--- php/application/models/users_model.php
+++ php/application/models/users_model.php
@@ -1,243 +1,147 @@
-	<?php 
+<?php
 
-	class Users_Model extends CI_Model
-	{
+class Users_Model extends CI_Model {
 
-		function __construct()
-		{
-			parent::__construct();
-		}
+    function __construct() {
+        parent::__construct();
+    }
 
-		function registerUser($user,$user_meta)
+    function registerUser($password, $email, $additional_data = array(), $group) {
+        //insert register
+        //$ip_address = $this->_prepare_ip($this->input->ip_address());
 
-		{
-			//insert register
-			$user_insert = array(
+        $user_insert = array(
+            "username" => strtolower($additional_data['first_name']).strtolower($additional_data['last_name']),
+            "password" => md5($password),
+            "email" => $email,
+            "ip_address" => $this->_prepare_ip($additional_data['ip_address']),
+            "created_on" => time(),
+            "last_login" => time(),
+            "first_name" => $additional_data['first_name'],
+            "last_name" => $additional_data['last_name'],
+            "phone" => $additional_data['mobile_number'],
+            "active" => '1'
+        );
 
-				"ip_address"=> (int) sprintf("%u\n", ip2long(long2ip(ip2long($this->input->ip_address())))),
-				"username"=> random_string('alnum',10),
-				"password"=> md5($user['password']),
-				"email"=>$user['email'],
-				"created_on" => time(),
-				"last_login" => time(),
-				"state"=> '1'
-				);
+        if ($this->email_check($email)) {
+            //$this->set_error('account_creation_duplicate_email');
+            // -1
+            return -1;
+        } elseif ($this->username_check($username)) {
+            //$this->set_error('account_creation_duplicate_username');
+            // -2
+            return -2;
+        } else {
+            $query = $this->db->insert('users', $user_insert);
+            $user_id = $this->db->insert_id();
 
-			$query = $this->db->insert('users',$user_insert);
-			$user_id = $this->db->insert_id();			
+            if ($query) {
 
-			if ($query)
-			{
+                $insert = array(
+                    "user_id" => $user_id,
+                    "group_id" => $group);
 
-				$insert = array(
-					"merchant_id" => '0',
-					"user_type"=> 'personal',
-					"firstname"=> $user_meta['firstname'],
-					"lastname"=> $user_meta['lastname'],
-					"earned_bonus"=> '0',
-					"expendable_bonus"=> '0',
-					"mobile_number"=> $user_meta['mobile_number'],
-					"current_latitude"=> $user_meta['lat'],
-					"current_longitude"=> $user_meta['lng'],
-					"referanskodu"=>random_string('unique',32),
-					"referans"=>$user_meta['referans']?$user_meta['referans']:"",
-					"user_id"=> $user_id,
-					"environment"=> $user_meta['environment']);
-				
-				$query = $this->db->insert('meta',$insert);
-				
-				return true;
-			}
+                $query = $this->db->insert('users_groups', $insert);
+                return $user_id;
+            } else {
+                return -3;
+            }
+        }
+    }
 
-			return false;
-		}
+    // 1-Public , 0-Protected
+    function getUserPrivacyState($user_id) {
+        $this->db->select('state');
+        $this->db->where('id', $user_id);
+        $query = $this->db->get('users');
+        $result = $query->row();
 
-		function registerSocialUser($user,$user_meta)
-		{
-			
-			//insert register
-			$user_insert = array(
-				"ip_address"=> (int) sprintf("%u\n", ip2long(long2ip(ip2long($this->input->ip_address())))),
-				"username"=> random_string('alnum',10),
-				"password"=> md5($user['password']?$user['password']:random_string('alnum',10)),
-				"email"=> $user['email'],
-				"created_on" => time(),
-				"last_login" => time(),
-				"state"=> '1');
-			
-			$query = $this->db->insert('users',$user_insert);
-			$user_id = $this->db->insert_id();			
+        return $result->state;
+    }
 
-			if ($query)
-			{
-				$insert = array(
-					"merchant_id" => '0',
-					"user_type"=> 'personal',
-					"firstname"=> $user_meta['firstname'],
-					"lastname"=> $user_meta['lastname'],
-					"earned_bonus"=> '0',
-					"expendable_bonus"=> '0',
-					"mobile_number"=> $user_meta['mobile_number']?$user_meta['mobile_number']:'',
-					"birthdate"=> $user_meta['birthdate'],
-					"gender"=> $user_meta['gender'],
-					"current_latitude"=> $user_meta['lat'],
-					"current_longitude"=> $user_meta['lng'],
-					"user_id"=> $user_id,
-					"referanskodu"=>random_string('unique',32),
-					"oauth_uid"=> $user_meta['facebook_id'],
-					"oauth_location"=> $user_meta['location'],
-					"profile_picture"=> $user_meta['profile_picture'],
-					"environment"=> $user_meta['environment']);
-				
-				$query = $this->db->insert('meta',$insert);
-			//	return $this->checkUser($user['email']);
+    public function email_check($email = '') {
 
-				return true;
-			}
+        if (empty($email)) {
+            return 0;
+        }
 
-			return false;
-		}
+        $query = $this->db->where('email', $email)
+                ->order_by("id", "ASC")
+                ->limit(1)
+                ->from('users')
+                ->count_all_results();
 
-		function updateSocialInfo($user_id, $user_meta)
-		{
-			
-			$user_meta = array(
-				"firstname"=> $user_meta['firstname'],
-				"lastname"=> $user_meta['lastname'],
-				"birthdate"=> $user_meta['birthdate'],
-				"gender"=> $user_meta['gender'],
-				"current_latitude"=> $user_meta['lat'],
-				"current_longitude"=> $user_meta['lng'],
-				"oauth_location"=> $user_meta['location'],
-				"oauth_uid"=> $user_meta['facebook_id'],
-				"profile_picture"=> $user_meta['profile_picture']
-				);
-			
-			//update event_user state
-			$query = $this->db->set($user_meta)
-			->where('user_id', $user_id)
-			->update('meta');	
+        if ($query > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
-			if ($query)
-			{
-				$this->db->set('last_login',time())
-				->where('id', $user_id)
-				->update('users');	
-				return true;
-			}
-			return false;
-		}
+    public function username_check($username = '') {
 
-		function updateLinkedSocialInfo($user_id, $user_meta)
-		{
-			//update event_user state
-			$query = $this->db->set($user_meta)
-			->where('user_id', $user_id)
-			->update('meta');	
+        if (empty($username)) {
+            return FALSE;
+        }
 
-			if ($query)
-			{
-				return true;
-			}
+        $query = $this->db->where('username', $username)
+                ->order_by("id", "ASC")
+                ->limit(1)
+                ->from('users')
+                ->count_all_results();
 
-			return false;
+        if ($query > FALSE) {
+            return TRUE;
+        }
 
-		}
+        return FALSE;
+    }
 
-		public function checkUser($email)
-		{
-			$this->db->select('u.id, u.email, m.firstname, m.lastname, u.group_id, u.password, m.earned_bonus, m.expendable_bonus, m.profile_picture, m.oauth_uid as facebook_id, m.merchant_id, m.mobile_number')
-			->from('users u')
-			->join('meta m', 'u.id = m.user_id')		           
-			->where('u.email', $email);
-			$query = $this->db->get();
-			if ( $query->num_rows > 0 )
-			{
-				$this->db->set('last_login',time())
-				->where('email', $email)
-				->update('users');	
+    protected function _prepare_ip($ip_address) {
+        //just return the string IP address now for better compatibility
+        return $ip_address;
+    }
 
-				$result = $query->result();
-				$query->free_result();
-				return $result;
-			}
-			return false;
-			
-		}
-		
-		public function checkUserId($id)
-		{
-			$this->db->select('u.id, u.email, m.firstname, m.lastname, u.password, m.earned_bonus, m.expendable_bonus, m.profile_picture, m.oauth_uid, m.city_id, m.county_id, m.district_id')
-			->from('users u')
-			->join('meta m', 'u.id = m.user_id')		           
-			->where('u.id', $id);
-			$query = $this->db->get();
-			if ( $query->num_rows > 0 )
-			{
-				$result = $query->result();
-				$query->free_result();
-				return $result;
-			}
-			return false;
-		}
-		
-		
-		public function checkUserSocial($facebook_id)
-		{
-			$this->db->select('u.id, u.email, um.firstname, um.lastname, um.earned_bonus, um.expendable_bonus, um.profile_picture, um.oauth_uid')
-			->from('meta um')	
-			->join('users u','um.user_id = u.id')	           
-			->where('um.oauth_uid', $facebook_id);
-			$query = $this->db->get();
-			if ( $query->num_rows > 0 )
-			{
-				$result = $query->result();
-				$query->free_result();
-				
-				return $result;
-			}
-			return false;
-		}
+    public function loginUser($email, $password) {
+        $password = md5($password);
 
-		public function checkUserLinkedSocial($facebook_id, $email)
-		{
-			$this->db->select('u.*')
-			->from('meta um')	
-			->join('users u','um.user_id = u.id')	           
-			->where('um.oauth_uid', $facebook_id);
-			$query = $this->db->get();
-			if ( $query->num_rows > 0 )
-			{
-				$result = $query->result();
-				$query->free_result();
-				
-				return $result;
-			}
-			$this->db->select('u.*')
-			->from('users u')	           
-			->where('u.email', $email);
-			$query = $this->db->get();
-			if ( $query->num_rows > 0 )
-			{
-				$result = $query->result();
-				$query->free_result();
-				return $result;
-			}
-			return 2;
-			
-		}
+        $query = $this->db->select('id,username,email,first_name,last_name,last_login')
+                ->where('email', $email)
+                ->where('password', $password)
+                ->limit(1)
+                ->order_by('id', 'desc')
+                ->get('users');
 
-         // 1-Public , 0-Protected
-		function getUserPrivacyState($user_id)
-		{    
-            $this->db->select('state');
-			$this->db->where('id',$user_id); 
-			$query = $this->db->get('users');  
-			$result = $query->row(); 
+        if ($query->num_rows() === 1) {
+            $updateData = array('last_login' => time(),
+                'active' => 1);
 
-			return $result->state;
+            $user = $query->row();
 
-		}
+            $this->db->where('id', $user->id);
+            $this->db->update('users', $updateData);
 
-	}
-	?>
+            return $user;
+        } else {
+            //There is no user, or password not matched
+            return NULL;
+        }
+    }
+
+    public function logoutUser($id) {
+
+        $updateData = array('active' => 0);
+
+        $query = $this->db->where('id', $id)
+                ->update('users', $updateData);
+
+        if ($query) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+}
+
+?>
